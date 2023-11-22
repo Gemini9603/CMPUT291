ea4b3d96d8f9716300e9a18a8d296621c0f6c0af
Pooja963 <92652617+Pooja963@users.noreply.github.com>
2023-11-22 04:14:16 -0700

    Add files via upload

---
+++ Mini Group Project 3/Q4A3.py
@@ -0,0 +1,194 @@
+import sqlite3
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+conn = None
+c = None
+
+def connect(path):
+    global conn, c
+    conn = sqlite3.connect(path)
+    c = conn.cursor()
+    conn.commit()
+    return
+
+def rename1():
+    global conn, c
+    c.execute('ALTER TABLE Customers RENAME TO orig_Customers;')
+    c.execute('ALTER TABLE Sellers RENAME TO orig_Sellers;')
+    c.execute('ALTER TABLE Orders RENAME TO orig_Orders;')
+    c.execute('ALTER TABLE Order_items RENAME TO orig_Order_items;')
+    conn.commit()
+    return
+
+def uninformed():
+    global conn, c
+    c.execute("PRAGMA foreign_keys=OFF;")
+    c.execute("PRAGMA synchronous = OFF")
+    c.execute("PRAGMA journal_mode = MEMORY")
+    c.execute("PRAGMA auto_vacuum = NONE")
+    c.execute("PRAGMA automatic_index = FALSE;")
+    c.execute('''CREATE TABLE Customers(
+                                        customer_id TEXT,                                   
+                                        customer_postal_code INTEGER
+                                       );
+             ''')
+    c.execute('''CREATE TABLE Sellers (
+                                       seller_id TEXT, 
+                                       seller_postal_code INTEGER
+                                      );
+             ''')
+    c.execute('''CREATE TABLE Orders (
+                                      order_id TEXT, 
+                                      customer_id TEXT
+                                     );
+             ''')
+    c.execute('''CREATE TABLE Order_items (
+                                        order_id TEXT,
+                                        order_item_id TEXT, 
+                                        product_id TEXT,
+                                        seller_id TEXT
+                                        );
+             ''')
+    conn.commit()
+    return
+
+def insert_values():
+    global conn, c
+    c.execute('INSERT INTO Customers SELECT * FROM orig_customers;')
+    c.execute('INSERT INTO Sellers SELECT * FROM orig_sellers;')
+    c.execute('INSERT INTO Orders SELECT * FROM orig_orders;')
+    c.execute('INSERT INTO Order_items SELECT * FROM orig_order_items;')
+    conn.commit()
+    return
+
+def drop_tables():
+    global conn, c
+    c.execute('DROP TABLE IF EXISTS Customers;')
+    c.execute('DROP TABLE IF EXISTS Sellers;')
+    c.execute('DROP TABLE IF EXISTS Orders;')
+    c.execute('DROP TABLE IF EXISTS Order_items;')
+    conn.commit()
+    return
+
+def rename2():
+    global conn, c
+    c.execute('ALTER TABLE orig_Customers RENAME TO Customers;')
+    c.execute('ALTER TABLE orig_Sellers RENAME TO Sellers;')
+    c.execute('ALTER TABLE orig_Orders RENAME TO Orders;')
+    c.execute('ALTER TABLE orig_Order_items RENAME TO Order_items;')
+    conn.commit()
+    return
+
+def self_optimized():
+    global conn, c
+    c.execute('PRAGMA foreign_keys=ON;')
+    c.execute('PRAGMA automatic_index = TRUE;')
+    conn.commit()
+    return
+
+def user_optimized():
+    global conn, c
+    c.execute('PRAGMA foreign_keys=ON;')
+    c.execute('CREATE INDEX sellerid ON Sellers(seller_id);')
+    c.execute('CREATE INDEX orderid ON Orders(order_id);')
+    c.execute('CREATE INDEX customerid ON Orders(customer_id);')
+    conn.commit()
+    return
+
+def drop_index():
+    global conn, c
+    c.execute('DROP INDEX sellerid;')
+    c.execute('DROP INDEX orderid;')
+    c.execute('DROP INDEX customerid;')
+    return
+
+def Q4():
+    global conn, c
+    c.execute('SELECT customer_id FROM Orders GROUP BY customer_id HAVING COUNT(*) > 1 ORDER BY random() LIMIT 1;')
+    rows = c.fetchall()
+    a = 0
+    total = 0
+    for i in rows:
+        cid = rows[a]
+        c.execute('''SELECT COUNT(DISTINCT Sellers.seller_postal_code)
+                     FROM Order_items
+                     INNER JOIN Sellers ON Sellers.seller_id=Order_items.seller_id
+                     INNER JOIN Orders ON Orders.order_id=Order_items.order_id
+                     INNER JOIN Customers ON Customers.customer_id=Orders.customer_id
+                     WHERE Customers.customer_id = ?;''', (cid))
+        a = a + 1
+        count = c.fetchone()
+        total = total + count[0]
+    conn.commit()
+    return
+
+def avg_time():
+    start = time.time()
+    for i in range(50):
+        Q4()
+    end = time.time()
+    avg = ((end-start)/50)* 10**3
+    print(avg)
+    return avg
+
+def create_graph(small, medium, large):
+    db = ("Small", "Medium", "Large")
+    small1 = np.fromiter(small, float)
+    medium1 = np.fromiter(medium, float)
+    large1 = np.fromiter(large, float)
+    avgtime = {
+                  "Uninformed": np.array([small1[0], medium1[0], large1[0]]),
+                  "Self-optimized": np.array([small1[1], medium1[1], large1[1]]),
+                  "User-optimized": np.array([small1[2], medium1[2], large1[2]])
+               }
+    width = 0.5
+    fig, ax = plt.subplots()
+    bottom = np.zeros(3)
+    for boolean, avgtime in avgtime.items():
+        p = ax.bar(db, avgtime, width, label=boolean, bottom=bottom)
+        bottom += avgtime
+    ax.set_title("Average time taken to run the three scenarios")
+    ax.legend(loc="upper left")
+    plt.xlabel("Databases")
+    plt.ylabel("Average Time")
+    plt.show()
+    return
+
+def database(size):
+    global conn, c
+    path = size
+    connect(path)
+    rename1()
+    uninformed()
+    insert_values()
+    a = avg_time()
+    drop_tables()
+    rename2()
+    conn.close()
+    path = size
+    connect(path)
+    self_optimized()
+    b = avg_time()
+    conn.close()
+    path = size
+    connect(path)
+    user_optimized()
+    d = avg_time()
+    drop_index()
+    conn.close()
+    return [a, b, d]
+
+def main():
+    global conn, c
+    size = "./A3Small.db"
+    small = database(size)
+    size = "./A3Medium.db"
+    medium = database(size)
+    size = "./A3Large.db"
+    large = database(size)
+    create_graph(small, medium, large)
+
+if __name__ == "__main__":
+    main()
